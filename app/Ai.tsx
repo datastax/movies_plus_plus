754b1541b32f031d745bbde315739c4256b31cf6
Tejas Kumar <tejas@tejas.qa>
2024-06-13 00:39:41 +0100

    Use langflow and show trailers

--- app/Ai.tsx
+++ app/Ai.tsx
@@ -5,34 +5,102 @@ import { createAI, getMutableAIState, streamUI } from "ai/rsc";
 import { openai } from "@ai-sdk/openai";
 import { z } from "zod";
 import { DataAPIClient } from "@datastax/astra-db-ts";
-import { Movie } from "./Movie";
-import { Movies } from "./Movies";
+import Markdown from "react-markdown";
+import ReactPlayer from "./Player";
 
 export const Ai = createAI({
   actions: {
     continueConversation: async ({ content }: any) => {
       const history = getMutableAIState();
       const result = await streamUI({
-        model: openai("gpt-3.5-turbo"),
+        model: openai("gpt-4o"),
         messages: [...history.get(), { role: "user", content }],
+        text: ({ content, done }) => {
+          if (done) {
+            history.done([...history.get(), { role: "assistant", content }]);
+          }
+          return content;
+        },
         tools: {
-          getMovies: {
+          showTrailer: {
+            description: "Show a trailer",
             parameters: z.object({
-              prompt: z.string(),
+              movieName: z.string(),
             }),
-            description: "Tell the user about movies that match the prompt",
-            generate: async function* ({ prompt }) {
-              yield "Searching...";
+            generate: async function* ({ movieName }) {
               const client = new DataAPIClient(
                 process.env.ASTRA_DB_APPLICATION_TOKEN!
               );
               const db = client.db(process.env.ASTRA_DB_API_ENDPOINT!);
-              const movies: any = await db
+              yield "Searching for movie " + movieName + "...";
+              const movie: any = await db
                 .collection("movies")
-                .find({}, { vectorize: prompt, limit: 8 })
-                .toArray();
+                .findOne({}, { vectorize: movieName });
 
-              return <Movies movies={movies} />;
+              yield "Found movie, getting trailer...";
+              const trailer = await fetch(
+                `https://api.themoviedb.org/3/movie/${movie._id}/videos?language=en-US&api_key=${process.env.TMDB_API_KEY}`
+              )
+                .then((r) => r.json())
+                .then((d) => d.results[0].key);
+
+              return (
+                <ReactPlayer
+                  playsinline
+                  muted
+                  width="100%"
+                  controls
+                  height="auto"
+                  style={{ height: "auto", aspectRatio: "16/9" }}
+                  playing
+                  url={`https://www.youtube-nocookie.com/embed/${trailer}`}
+                />
+              );
+            },
+          },
+          getMovies: {
+            parameters: z.object({
+              prompt: z.string(),
+            }),
+            description: "Get a list of movies.",
+            generate: async function* ({ prompt }) {
+              yield "Searching...";
+
+              const movies = await fetch(
+                "http://127.0.0.1:7864/api/v1/run/e291a07a-4335-435b-9da5-eb8508b9c4aa?stream=false",
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    "Cache-Control": "no-cache",
+                  },
+                  body: JSON.stringify({
+                    input_value: prompt,
+                  }),
+                }
+              )
+                .then((r) => r.json())
+                .then((d) => {
+                  console.dir({ d }, { depth: Infinity });
+                  return d.outputs[0].outputs[0].results.result;
+                });
+
+              return (
+                <div className="grid gap-4">
+                  <Markdown
+                    components={{
+                      ul: ({ children }) => (
+                        <ul className="grid gap-2">{children}</ul>
+                      ),
+                      ol: ({ children }) => (
+                        <ol className="grid gap-2">{children}</ol>
+                      ),
+                    }}
+                  >
+                    {movies}
+                  </Markdown>
+                </div>
+              );
             },
           },
         },
--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,17 @@ export default function Home() {
         !result.length ? "flex items-center h-screen" : ""
       }`}
     >
-      <header className="flex w-full items-center gap-32 px-8">
+      <header
+        className={`${
+          result.length > 0 ? "fixed top-0" : ""
+        } flex bg-black bg-opacity-90 backdrop-blur-lg w-full items-center gap-32 p-8`}
+      >
         <Logo />
         <SearchForm shouldShowSuggestions={!result.length} />
       </header>
+      {result.length > 0 && <div className="h-[58px]"></div>}
       {result.length > 0 && (
-        <div className="p-8 grid gap-8 grid-cols-4">
+        <div className="p-8 grid gap-8">
           {result.map((m: any) => m.display)}
         </div>
       )}
