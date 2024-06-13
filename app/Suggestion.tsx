dbc5fe7903e41ce5b9a9029965cd598919b8189c
Tejas Kumar <tejas@tejas.qa>
2024-06-13 13:04:00 +0100

    Make suggestions interactive

--- app/Suggestion.tsx
+++ app/Suggestion.tsx
@@ -6,7 +6,7 @@ export function Suggestion({ children }: PropsWithChildren) {
 
   return (
     <button
-      className="bg-[#250e09] rounded-lg text-[#F45D3A] p-4"
+      className="bg-[#250e09] rounded-lg text-[#F45D3A] transition-all hover:bg-[#4e1e13] hover:text-white p-4"
       onClick={() => search(children as string)}
       type="button"
     >
