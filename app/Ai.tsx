9e101e9191ed16e883ce0c2ca5fc5df913ca9e6d
Tejas Kumar <tejas@tejas.qa>
2024-06-13 13:05:01 +0100

    Use langflow for list, posters, and trailer

--- app/Ai.tsx
+++ app/Ai.tsx
@@ -5,31 +5,133 @@ import { createAI, getMutableAIState, streamUI } from "ai/rsc";
 import { openai } from "@ai-sdk/openai";
 import { z } from "zod";
 import { DataAPIClient } from "@datastax/astra-db-ts";
-import { Movie } from "./Movie";
 import { Movies } from "./Movies";
+import { IntegrationSpinner } from "./IntegrationSpinner";
+import { Player } from "./Player";
+import { Markdown } from "./Markdown";
+
+let lastLangflowResponse = "";
 
 export const Ai = createAI({
   actions: {
     continueConversation: async ({ content }: any) => {
       const history = getMutableAIState();
       const result = await streamUI({
-        model: openai("gpt-3.5-turbo"),
+        model: openai("gpt-4o"),
         messages: [...history.get(), { role: "user", content }],
+        text: ({ content, done }) => {
+          if (done) {
+            history.done([...history.get(), { role: "assistant", content }]);
+          }
+          return <Markdown>{content}</Markdown>;
+        },
         tools: {
           getMovies: {
+            description:
+              "A tool used to get a list of movies matching a user's query",
             parameters: z.object({
-              prompt: z.string(),
+              query: z.string(),
             }),
-            description: "Tell the user about movies that match the prompt",
-            generate: async function* ({ prompt }) {
-              yield "Searching...";
+            generate: async function* ({ query }) {
+              console.log("Looking for movies", { query });
+              yield (
+                <div className="flex items-center gap-4">
+                  <IntegrationSpinner /> Asking Langflow...
+                </div>
+              );
+              const movies = await fetch(
+                "http://127.0.0.1:7863/api/v1/run/e291a07a-4335-435b-9da5-eb8508b9c4aa?stream=false",
+                {
+                  method: "post",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    input_value: query,
+                  }),
+                }
+              )
+                .then((r) => r.json())
+                .then((d) => d.outputs[0].outputs[0].results.result);
+
+              lastLangflowResponse = movies;
+
+              return <Markdown>{movies}</Markdown>;
+            },
+          },
+          showTrailer: {
+            description: "When the user asks for a trailer, use this tool",
+            parameters: z.object({
+              movieName: z.string(),
+            }),
+            generate: async function* ({ movieName }) {
+              const client = new DataAPIClient(
+                process.env.ASTRA_DB_APPLICATION_TOKEN!
+              );
+              const db = client.db(process.env.ASTRA_DB_API_ENDPOINT!);
+
+              yield (
+                <div className="flex items-center gap-4">
+                  <IntegrationSpinner /> Getting more info about {movieName}...
+                </div>
+              );
+              const movie: any = await db
+                .collection("movies")
+                .findOne({}, { vectorize: movieName });
+
+              yield;
+              <div className="flex items-center gap-4">
+                <IntegrationSpinner /> Found movie, getting trailer...
+              </div>;
+              const trailer = await fetch(
+                `https://api.themoviedb.org/3/movie/${movie._id}/videos?language=en-US&api_key=${process.env.TMDB_API_KEY}`
+              )
+                .then((r) => r.json())
+                .then((d) => d.results[0].key);
+
+              return (
+                <Player
+                  playsinline
+                  muted
+                  width="100%"
+                  controls
+                  height="auto"
+                  style={{ height: "auto", aspectRatio: "16/9" }}
+                  playing
+                  url={`https://www.youtube-nocookie.com/embed/${trailer}`}
+                />
+              );
+            },
+          },
+          createGenerativeUi: {
+            parameters: z.object({
+              moviesFromContext: z
+                .string()
+                .describe(
+                  "A comma separated list of movies from the chat context"
+                ),
+            }),
+            description:
+              "A tool used to generate UI in response to a user explicitly asking for UI",
+            generate: async function* () {
+              yield (
+                <div className="flex items-center gap-4">
+                  <IntegrationSpinner /> Getting movie posters...
+                </div>
+              );
               const client = new DataAPIClient(
                 process.env.ASTRA_DB_APPLICATION_TOKEN!
               );
               const db = client.db(process.env.ASTRA_DB_API_ENDPOINT!);
               const movies: any = await db
                 .collection("movies")
-                .find({}, { vectorize: prompt, limit: 8 })
+                .find(
+                  {},
+                  {
+                    vectorize: lastLangflowResponse,
+                    limit: 8,
+                  }
+                )
                 .toArray();
 
               return <Movies movies={movies} />;
