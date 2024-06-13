dee37252c43366c9d3df3c4a80adb7523d813136
Tejas Kumar <tejas@tejas.qa>
2024-06-13 13:04:31 +0100

    Add dedicated reusable markdown formatter

---
+++ app/Markdown.tsx
@@ -0,0 +1,18 @@
+import ReactMarkdown from "react-markdown";
+
+export function Markdown({ children }: { children: string }) {
+  return (
+    <ReactMarkdown
+      components={{
+        ol: ({ children }) => (
+          <ol className="list-decimal grid gap-4">{children}</ol>
+        ),
+        ul: ({ children }) => (
+          <ul className="list-inside grid gap-4">{children}</ul>
+        ),
+      }}
+    >
+      {children}
+    </ReactMarkdown>
+  );
+}
