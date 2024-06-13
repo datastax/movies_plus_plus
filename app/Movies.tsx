d07f5b76e057d1779a331e1e1be6373a4af399cf
Tejas Kumar <tejas@tejas.qa>
2024-06-13 13:04:24 +0100

    Add grid to movies

--- app/Movies.tsx
+++ app/Movies.tsx
@@ -9,21 +9,27 @@ type Props = {
 
 export function Movies({ movies }: Props) {
   const [hoveredMovieIndex, setHoveredMovieIndex] = useState(-1);
-  return movies.map((m, i) => (
-    <a
-      onMouseEnter={() => setHoveredMovieIndex(i)}
-      onMouseLeave={() => setHoveredMovieIndex(-1)}
-      className={`${
-        hoveredMovieIndex !== -1 && hoveredMovieIndex !== i ? "opacity-50" : ""
-      } transition-all`}
-      key={m._id}
-      target="_blank"
-      href={`https://www.themoviedb.org/movie/${m._id}}`}
-    >
-      <Movie
-        title={m.title}
-        posterUrl={`https://image.tmdb.org/t/p/w500${m.poster_path}`}
-      />
-    </a>
-  ));
+  return (
+    <div className="grid grid-cols-8 gap-8">
+      {movies.map((m, i) => (
+        <a
+          onMouseEnter={() => setHoveredMovieIndex(i)}
+          onMouseLeave={() => setHoveredMovieIndex(-1)}
+          className={`${
+            hoveredMovieIndex !== -1 && hoveredMovieIndex !== i
+              ? "opacity-50"
+              : ""
+          } transition-all`}
+          key={m._id}
+          target="_blank"
+          href={`https://www.themoviedb.org/movie/${m._id}}`}
+        >
+          <Movie
+            title={m.title}
+            posterUrl={`https://image.tmdb.org/t/p/w500${m.poster_path}`}
+          />
+        </a>
+      ))}
+    </div>
+  );
 }
