f7f2a08168f1280a33147e2b321e7c4a16e29ee9
SonicDMG <david.gilardi@datastax.com>
2024-07-16 11:52:22 -0400

    Removing references to python scripts in ROOT

--- scrub.py
+++
@@ -1,8 +0,0 @@
-def scrub(content):
-  content = content.replace("What's your", "")
-  content = content.replace("Login to use TMDB's new rating system.", "")
-  content = content.replace("Welcome to Vibes, TMDB's new rating system! For more information, visit the  contribution bible.", "")
-  content = content.replace("Looks like we're missing the following data in en-US or en-US...", "")
-  content = content.replace("Login to edit", "")
-  content = content.replace("Login to report an issue", "")
-  return content
