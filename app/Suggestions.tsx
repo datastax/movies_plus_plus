25840ba4715da5f91e4469cd24ac3f06a8d2b43a
Arsalan <arsalankhan871483@gmail.com>
2024-07-24 19:40:32 +0500

    fix ui

--- app/Suggestions.tsx
+++ app/Suggestions.tsx
@@ -3,12 +3,16 @@ import { jetbrainsMono } from "./fonts";
 
 export function Suggestions() {
    return (
-      <div
-         className={`grid sm-flex  overflow-x-auto md:grid-cols-3 gap-4 ${jetbrainsMono.className}`}
-      >
-         <Suggestion>Scary movies set in the woods</Suggestion>
-         <Suggestion>Movies that feature large monsters</Suggestion>
-         <Suggestion>Movies with a strong female lead</Suggestion>
+      <div className=" overflow-x-auto lg:overflow-x-hidden ">
+         <div className="min-w-[1000px] sm:min-w-full">
+            <div
+               className={`grid grid-cols-3 gap-4 ${jetbrainsMono.className}`}
+            >
+               <Suggestion>Scary movies set in the woods</Suggestion>
+               <Suggestion>Movies that feature large monsters</Suggestion>
+               <Suggestion>Movies with a strong female lead</Suggestion>
+            </div>
+         </div>
       </div>
    );
 }
