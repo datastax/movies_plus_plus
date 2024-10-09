a8bea07fcffdef26a1b27901604309d38796db34
Tejas Kumar <tejas@tejas.qa>
2024-10-08 22:54:55 -0400

    Add map

--- app/Map.tsx
+++ app/Map.tsx
@@ -1,7 +1,7 @@
 export function Map() {
   return (
     <iframe
-      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAm2Yp0P-RH1yzmKB-l3JtCL4Nh84y59RU&q=movie+theaters+near+340+W+50th+St"
+      src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyAm2Yp0P-RH1yzmKB-l3JtCL4Nh84y59RU&origin=New World Stages, 340 W 50th St, New York, NY 10019&mode=walking&destination=movie+theaters+near+340+W+50th+St"
       style={{ border: 0 }}
       allowFullScreen
       loading="lazy"
